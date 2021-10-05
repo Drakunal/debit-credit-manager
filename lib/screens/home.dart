f8d55b41bd15bd50d1db60698b54c889abebe902
Drakunal <drakunaldey1@gmail.com>
2021-10-05 10:12:45 +0530

    bottom navigation bar added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -14,6 +15,18 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text("Savings++"),
       ),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.blueAccent,
+        items: <Widget>[
+          Icon(Icons.add, size: 30),
+          Icon(Icons.list, size: 30),
+          Icon(Icons.compare_arrows, size: 30),
+        ],
+        onTap: (index) {
+          //Handle button tap
+        },
+      ),
+      body: Container(color: Colors.blueAccent),
     );
   }
 }
