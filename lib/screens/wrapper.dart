7838fcd29a732a5f15cf1f986779a7b7678692a6
Drakunal <drakunaldey1@gmail.com>
2021-10-05 20:14:57 +0530

    login done

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,4 +1,6 @@
+import 'package:debit_credit/screens/home.dart';
 import 'package:debit_credit/screens/signin.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Wrapper extends StatefulWidget {
@@ -11,6 +13,23 @@ class Wrapper extends StatefulWidget {
 class _WrapperState extends State<Wrapper> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SignIn());
+    return Scaffold(
+        body: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text("Error Occured!"),
+                );
+              } else if (snapshot.hasData) {
+                return Home();
+              } else {
+                return SignIn();
+              }
+            }));
   }
 }
