bb2db5fe9148a7fdabc94884f97b0c259f988749
Drakunal <drakunaldey1@gmail.com>
2021-10-05 19:52:14 +0530

    list of users shown in google

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:debit_credit/screens/home.dart';
 import 'package:debit_credit/screens/signin.dart';
+import 'package:debit_credit/services/authenticate.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
-  runApp(MaterialApp(
-    home: SignIn(),
-  ));
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => Authenticate(),
+        child: MaterialApp(
+          home: SignIn(),
+        ),
+      );
 }
--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -1,5 +1,7 @@
+import 'package:debit_credit/services/authenticate.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
@@ -34,7 +36,11 @@ class _SignInState extends State<SignIn> {
                       padding: EdgeInsets.fromLTRB(20, 8, 20, 8),
                       primary: Colors.white,
                       onPrimary: Colors.black),
-                  onPressed: _a,
+                  onPressed: () {
+                    final provider =
+                        Provider.of<Authenticate>(context, listen: false);
+                    provider.googleLogin();
+                  },
                   icon: FaIcon(
                     FontAwesomeIcons.google,
                     color: Colors.red,
