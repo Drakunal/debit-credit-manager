e497252fd27bf97b951a2a1ef17d451d63313a49
Drakunal <drakunaldey1@gmail.com>
2021-10-06 21:18:27 +0530

    form for addition created

--- lib/screens/addition_page.dart
+++ lib/screens/addition_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:date_field/date_field.dart';
 
 class Addition extends StatefulWidget {
   const Addition({Key? key}) : super(key: key);
@@ -8,12 +9,87 @@ class Addition extends StatefulWidget {
 }
 
 class _AdditionState extends State<Addition> {
+  final _formKey = GlobalKey<FormState>();
+  String _task = "";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(),
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: Container(
+        child: Center(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Text("Add New Transaction Here"),
+                Spacer(),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintStyle: TextStyle(color: Colors.black45),
+                    errorStyle: TextStyle(color: Colors.redAccent),
+                    border: OutlineInputBorder(),
+                    suffixIcon: Icon(Icons.money),
+                    labelText: 'Transaction Details',
+                  ),
+                  // initialValue: userData!.name,
+                  // userData!.name
+                  // decoration: textInputDecoration.copyWith(
+                  //   hintText: 'Name',
+                  // ),
+                  validator: (value) =>
+                      value!.isEmpty ? 'Enter transaction details' : null,
+                  onChanged: (value) {
+                    setState(() {
+                      _task = value;
+                    });
+                  },
+                ),
+                Spacer(),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintStyle: TextStyle(color: Colors.black45),
+                    errorStyle: TextStyle(color: Colors.redAccent),
+                    border: OutlineInputBorder(),
+                    suffixIcon: Icon(Icons.money),
+                    labelText: 'Transaction amount',
+                  ),
+                  keyboardType: TextInputType.number,
+                  // initialValue: userData!.name,
+                  // userData!.name
+                  // decoration: textInputDecoration.copyWith(
+                  //   hintText: 'Name',
+                  // ),
+
+                  validator: (value) =>
+                      value!.isEmpty ? 'Enter the amount' : null,
+                  onChanged: (value) {
+                    setState(() {
+                      _task = value;
+                    });
+                  },
+                ),
+                Spacer(),
+                DateTimeFormField(
+                  decoration: const InputDecoration(
+                    hintStyle: TextStyle(color: Colors.black45),
+                    errorStyle: TextStyle(color: Colors.redAccent),
+                    border: OutlineInputBorder(),
+                    suffixIcon: Icon(Icons.event_note),
+                    labelText: 'Transaction Date',
+                  ),
+                  mode: DateTimeFieldPickerMode.date,
+                  autovalidateMode: AutovalidateMode.always,
+                  // validator: (e) =>
+                  //     (e?.day ?? 0) == 1 ? 'Please not the first day' : null,
+                  onDateSelected: (DateTime value) {
+                    print(value);
+                  },
+                ),
+                Spacer(),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
